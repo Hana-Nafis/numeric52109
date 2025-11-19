688ed44f38e6abce5a701ecd7f662ea1c3dcb44d
Hana Nafis <hn602@bath.ac.uk>
2025-11-19 20:19:40 +0000

    Fix test_sp.py error: corrected function call/message

--- test_sp.py
+++ test_sp.py
@@ -4,14 +4,23 @@
 ###
 
 import simple_package as sp
+import numpy as np
 
 if __name__ == '__main__':
     ## Define two numbers
     a = 1;
     b = 2;
     
-    ## Print their sum with a nice message.
-    print(f"The sum of {a} and {b} is {a + b}")
+    # Test basic operations
+    print(f"The sum of {a} and {b} is {sp.add(a,b)}")
+    print(f"The product of {a} and {b} is {sp.multiply(a,b)}")
 
-    ## Now do the same for the function in sp
-    print(f"The product of {a} and {b} is {sp.add(a,b)}")
+    # Test complex operations
+    print(f"The sine of {a} is {sp.sine(a)}")
+    print(f"The logarithm of {b} is {sp.logarithm(b)}")
+
+    # Test statistics functions
+    data = np.random.randint(0,101,100)
+    sp.print_statistics(data)
+    sp.plot_statistics(data)
+    
